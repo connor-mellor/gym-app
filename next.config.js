58fe5fb4d34d80e090c1357d1e13cfe9f80080c5
Connor-Mellor <Connor.Mellor@frasers.group>
2023-04-11 09:51:19 +0100

    fix: added basePath to nextJSconfig.. again

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/gym-app',
+  basePath: '/gym-app/',
+  assetPrefix: '/gym-app/',
 }
 
 module.exports = nextConfig
--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -21,7 +21,7 @@ export function Navigation() {
                 <div className={classes.logoContainer}>
                   <Image
                     loader={myLoader}
-                    src="/muscle-flex.svg"
+                    src={process.env.assetPrefix + '/muscle-flex.svg'}
                     alt="muscle flex logo"
                     className={classes.logo}
                     width={45}
