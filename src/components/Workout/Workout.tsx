d74bf7be5f317ecc0e96f4ebb679704992e32a8c
Connor-Mellor <Connor.Mellor@frasers.group>
2023-04-18 16:06:14 +0100

    big push

--- src/components/Workout/Workout.tsx
+++ src/components/Workout/Workout.tsx
@@ -1,23 +1,26 @@
 import { Grid } from "@material-ui/core";
 import { BasicTable } from "../BasicTable";
 
-export function Workout({ id, workoutName, sets }: any) {
-    console.log(sets)
+export function Workout({ workout: { id, workoutName, sets } }: any) {
     return (
-        <>
-            <Grid container>
-                <Grid item xs={6}>
-                    <span>{ id + '.' }</span>
-                    <h3>{ workoutName }</h3>
-                </Grid>
-                <Grid item xs={4}>
-                    <p>Total weight: ???kg</p>
-                </Grid>
-                <Grid item xs={2}>
-                    <button>Remove Workout</button>
-                </Grid>
+        <Grid container spacing={2}>
+            <Grid item xs={4}>
+                <h3>
+                    <span>
+                        { id + '. ' }
+                    </span>
+                    { workoutName }
+                </h3>
             </Grid>
-            <BasicTable sets={sets} />
-        </>
+            <Grid item xs={4}>
+                <p>Total weight: ???kg</p>
+            </Grid>
+            <Grid item xs={4}>
+                <button>Remove Workout</button>
+            </Grid>
+            <Grid item xs={12}>
+                <BasicTable sets={sets} />
+            </Grid>
+        </Grid>
     )
 }
--- src/components/WorkoutSession/WorkoutSession.tsx
+++ src/components/WorkoutSession/WorkoutSession.tsx
@@ -7,15 +7,15 @@ import { Workout } from "../Workout/index"
 //     workouts: any[]
 // }
 
-export function WorkoutSession({ workoutSession: { id, sessionName, sessionDescription, workouts } }: any, index: any) {
-    console.log(workouts)
+export function WorkoutSession({ workoutSession: { id, sessionName, sessionDescription, workouts } }: any) {
     return (
         <>
             <h3>{ id + '. ' + sessionName }</h3>
             <p>{ sessionDescription }</p>
-            {/* {workouts.map((workout: any) => (
+
+            {workouts.map((workout: any, index: number) => (
                 <Workout key={index} workout={workout} />
-            ))} */}
+            ))}
         </>
     )
 }
