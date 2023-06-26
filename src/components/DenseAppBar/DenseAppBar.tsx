7cd17b3b5b858625e9b736372afb72d4d5978687
Connor-Mellor <Connor.Mellor@frasers.group>
2023-06-26 16:14:03 +0100

    updating nextjs

--- src/components/DenseAppBar/DenseAppBar.tsx
+++ src/components/DenseAppBar/DenseAppBar.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import Image from "next/image";
+import Image from "next/legacy/image";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import Image from "next/legacy/image";
 import classes from "./Navigation.module.css";
 import Link from "next/link";
 import { Burger, Menu } from "..";
